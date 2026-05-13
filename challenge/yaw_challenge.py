aa9d52b5db42961472f30c026af00d583c421868
oluwafemi 🍀 <39830671+emmydex@users.noreply.github.com>
2026-05-13 17:52:09 -0400

    a mini challenge

---
+++ challenge/yaw_challenge.py
@@ -0,0 +1,71 @@
+# the goal is to try diffrent yaw angles and the diffrence 
+# between orientation and movemnt frame
+
+import airsim
+import time
+
+#connectin to airsim
+client= airsim.MultirotorClient()
+client.confirmConnection()
+print("connected")
+
+#api enabled
+client.enableApiControl(True)
+
+#arm drone
+client.armDisarm(True)
+
+#taking off
+client.takeoffAsync().join()
+print("we in the sky")
+
+time.sleep(2)
+
+# upward movement
+client.moveToZAsync(-20, 2).join()
+print("reached altitude")
+
+# rotate 90 degrees yaw control
+print("rotating ...")
+client.rotateToYawAsync(45).join()
+
+time.sleep(2)
+
+#moving forward
+print("moving forward..")
+client.moveByVelocityAsync(5,0,0,3).join()
+
+client.hoverAsync().join()
+
+time.sleep(2)
+
+# another rotation
+print("rotating again")
+client.rotateToYawAsync(-180).join()
+
+# another rotation
+print("rotating again")
+client.rotateToYawAsync(270).join()
+
+# moving forward again
+print("another forward movemnt")
+client.moveByVelocityAsync(5,0,0,3).join()
+
+client.hoverAsync().join()
+
+time.sleep(2)
+
+
+
+#landing
+print("landing ...")
+client.landAsync().join()
+
+#disarm
+client.armDisarm(False)
+
+
+# realease control
+client.enableApiControl(False)
+
+print("mission was a success!") 
