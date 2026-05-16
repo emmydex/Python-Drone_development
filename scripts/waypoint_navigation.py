a28d02c449187730e7c20a121b3482cb953f66c3
oluwafemi 🍀 <39830671+emmydex@users.noreply.github.com>
2026-05-16 08:56:30 -0400

    navigation testinig

--- scripts/waypoint_navigation.py
+++ scripts/waypoint_navigation.py
@@ -24,3 +24,42 @@ time.sleep(2)
 client.moveToZAsync(-10, 2).join()
 print("reached altitude")
 
+# waypoint 1
+print("moving to waypoint 1")
+client.moveToPositionAsync(20,0,-10,5).join()
+
+time.sleep(2)
+
+
+# waypoint 2
+print("moving to waypoint 2")
+client.moveToPositionAsync(20,20,-10,5).join()
+
+time.sleep(2)
+
+
+# waypoint 3
+print("moving to waypoint 3")
+client.moveToPositionAsync(0,20,-10,5).join()
+
+time.sleep(2)
+
+
+# return close to home
+print("returning home")
+client.moveToPositionAsync(0,0,-10,5).join()
+
+time.sleep(2)
+
+#land
+print("landing")
+client.landAsync().join()
+
+
+# disarm
+client.armDisarm(False)
+
+# disable api
+client.enableApiControl(False)
+
+print("mission complete")
