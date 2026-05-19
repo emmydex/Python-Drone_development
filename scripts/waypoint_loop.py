b69c8eb400c8aef49fcfd379da59a2b1ba09e1a1
oluwafemi 🍀 <39830671+emmydex@users.noreply.github.com>
2026-05-19 17:19:08 -0400

    loops

---
+++ scripts/waypoint_loop.py
@@ -0,0 +1,50 @@
+import airsim
+import time
+
+client = airsim.MultirotorClient()
+client.confirmConnection()
+
+# connect to api
+client.enableApiControl(True)
+
+# arm drone
+client.armDisarm(True)
+
+print("starting mission")
+
+client.takeoffAsync().join()
+
+# moving to altitiude
+client.moveToZAsync(-10, 2).join()
+
+# adding waypoints to a list
+
+waypoints = [
+    (20, 0, -10),
+    (20, 20, -10),
+    (0, 20, -10),
+    (0, 0, -10)
+]
+
+# looping through waypoints
+for point in waypoints:
+    x, y, z = point
+    print(f"reaching first milestone {point}")
+    client.moveToPositionAsync(x, y, z, 5).join()
+
+    client.hoverAsync().join()
+    time.sleep(1)
+
+
+# landing
+print("returning to start point")
+client.landAsync().join()
+
+# disarming
+client.armDisarm(False)
+
+
+# removing Api control
+client.enableApiControl(False)
+
+print("mission complete")
