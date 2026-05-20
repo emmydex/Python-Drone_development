c53c0a957ca0d9ebab698e57d1bff5b6e7b65f5b
oluwafemi 🍀 <39830671+emmydex@users.noreply.github.com>
2026-05-20 19:54:09 -0400

    if statement

--- scripts/autonomous_decision.py
+++ scripts/autonomous_decision.py
@@ -31,3 +31,36 @@ position = state.kinematics_estimated.position
 current_altitude = position.z_val
 
 print(f"current altitude: {current_altitude}")
+
+# decision making based on altitude
+if current_altitude > -10:
+    print("altitude tooo low!")
+
+    print("climbing higher...")
+
+    client.moveToZAsync(-15, 3).join()
+    print("safe altitude achieved!")
+
+else:
+
+    print("altitude is safe.")
+
+time.sleep(2)
+
+# hovering
+
+client.hoverAsync().join()
+
+time.sleep(2)
+
+# landing
+print("touching down...")
+client.landAsync().join()
+
+#disarm
+client.armDisarm(False)
+
+#release api control
+client.enableApiControl(False)
+
+print("mission success")
