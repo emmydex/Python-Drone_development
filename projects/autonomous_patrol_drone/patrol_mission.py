e50e2fed3e64c96fc4c1715667090ee8f99c9680
oluwafemi 🍀 <39830671+emmydex@users.noreply.github.com>
2026-05-28 14:08:47 -0400

    decision logic should be inside of the loop

--- projects/autonomous_patrol_drone/patrol_mission.py
+++ projects/autonomous_patrol_drone/patrol_mission.py
@@ -15,8 +15,13 @@ client.armDisarm(True)
 # takeoff
 client.takeoffAsync().join()
 
-# base altitude
-client.moveToZAsync(-10, 3).join()
+# base altitude and this should be like -5 for all waypoint altitude is -10
+client.moveToZAsync(-5, 2).join()
+
+
+#adding a foward movement to test the decision making part of the project
+client.moveToPositionAsync(5, 0, -10, 2).join()
+
 
 waypoints = [
 
@@ -43,29 +48,10 @@ for i in range(20):
     # getting drone position
     position = state.kinematics_estimated.position
 
-    # getting drone velocity
-    velocity = state.kinematics_estimated.linear_velocity
+    # current altitude
+    current_altitude = position.z_val
 
-    print("\n-- DATA FOR THIS PATROL DRONE--")
-
-    # getting position data
-    print(f"x position: {position.x_val:.2f}")
-    print(f"y position: {position.y_val:.2f}")
-    print(f"z position: {position.z_val:.2f}")
-
-    # getting velocity data
-    print(f"x velocity: {velocity.x_val:.2f}")
-    print(f"y velocity: {velocity.y_val:.2f}")
-    print(f"z velocity: {velocity.z_val:.2f}")
-
-
-
-time.sleep(1)
-
-# current altitude
-current_altitude = position.z_val
-
-print(f"current altitude: {current_altitude}")
+    print(f"current altitude: {current_altitude}")
 
 # decision making based on altitude
 if current_altitude > -10:
@@ -81,6 +67,27 @@ else:
     print("current altitude is safe.")
 
 time.sleep(1)
+
+    # getting drone velocity
+velocity = state.kinematics_estimated.linear_velocity
+
+print("\n-- DATA FOR THIS PATROL DRONE--")
+
+    # getting position data
+print(f"x position: {position.x_val:.2f}")
+print(f"y position: {position.y_val:.2f}")
+print(f"z position: {position.z_val:.2f}")
+
+    # getting velocity data
+print(f"x velocity: {velocity.x_val:.2f}")
+print(f"y velocity: {velocity.y_val:.2f}")
+print(f"z velocity: {velocity.z_val:.2f}")
+
+
+
+time.sleep(1)
+
+
 
 # hovering
 client.hoverAsync().join()
