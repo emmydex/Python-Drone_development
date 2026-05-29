920a58885efaf0a7257ea0a63ab7855b03f78d2c
oluwafemi 🍀 <39830671+emmydex@users.noreply.github.com>
2026-05-29 19:19:21 -0400

    update

--- projects/telemetry_dashboard/telemetry_dashboard.py
+++ projects/telemetry_dashboard/telemetry_dashboard.py
@@ -18,3 +18,27 @@ client.takeoffAsync().join()
 
 #move to monitoring altitude
 client.moveToZAsync(-15,4).join()
+
+print("reached monitoring altitude")
+
+
+print("\nStarting telemetry monitoring..\n")
+
+for i in range(20):
+
+    state = client.getMultirotorState()
+    position = state.kinematics_estimated.position
+
+    velocity = state.kinematics_estimated.linear_velocity
+
+    print(f"\n--- Telemetry Update {i+1} ---")
+
+    print(f"x position: {position.x_val:.2f}")
+    print(f"y position: {position.y_val:.2f}")
+    print(f"z position: {position.z_val:.2f}")
+
+    print(f"x velocity: {velocity.x_val:.2f}")
+    print(f"y velocity: {velocity.y_val:.2f}")
+    print(f"z velocity: {velocity.z_val:.2f}")
+
+    time.sleep(1)
