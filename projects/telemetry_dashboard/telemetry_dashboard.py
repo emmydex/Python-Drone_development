1d30e4bedb5e981d6411ec95dee91c2d08f56287
oluwafemi 🍀 <39830671+emmydex@users.noreply.github.com>
2026-05-29 19:02:41 -0400

    update

--- projects/telemetry_dashboard/telemetry_dashboard.py
+++ projects/telemetry_dashboard/telemetry_dashboard.py
@@ -4,4 +4,17 @@ import time
 # connectng to airsim
 client = airsim.MultirotorClient()
 client.confirmConnection()
+print("connected sucessfully")
 
+
+#enabling api control
+client.enableApiControl(True)
+
+# arming
+client.armDisarm(True)
+
+# takeoff
+client.takeoffAsync().join()
+
+#move to monitoring altitude
+client.moveToZAsync(-15,4).join()
