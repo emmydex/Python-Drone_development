dc635fefbe293755939e969f2af59eee2b801c8b
oluwafemi 🍀 <39830671+emmydex@users.noreply.github.com>
2026-06-03 09:02:06 -0400

    updated absolute value as drone could be moving sideways

---
+++ projects/telemetry_dashboard/telemetry_dashboard_v3.py
@@ -0,0 +1,71 @@
+import airsim
+import time
+
+# connectng to airsim
+client = airsim.MultirotorClient()
+client.confirmConnection()
+print("connected sucessfully")
+
+
+#enabling api control
+client.enableApiControl(True)
+
+# arming
+client.armDisarm(True)
+
+# takeoff
+client.takeoffAsync().join()
+
+#move to monitoring altitude
+client.moveToZAsync(-15,4).join()
+
+print("reached monitoring altitude")
+
+client.moveToPositionAsync(10, 0, -15, 3).join()
+
+print("\nStarting telemetry monitoring..\n")
+
+for i in range(20):
+
+    state = client.getMultirotorState()
+    position = state.kinematics_estimated.position
+
+    velocity = state.kinematics_estimated.linear_velocity
+
+    x = position.x_val
+    y = position.y_val
+    z = position.z_val
+
+    vx = velocity.x_val
+    vy = velocity.y_val
+    vz = velocity.z_val
+
+    print("\n==========================")
+    print(" 📊 TELEMETRY DASHBOARD")
+    print("============================")
+
+    print(f"x position: {x:.2f}")
+    print(f"y position: {y:.2f}")
+    print(f"z position: {z:.2f}")
+
+    print(f"x velocity: {vx:.2f}")
+    print(f"y velocity: {vy:.2f}")
+    print(f"z velocity: {vz:.2f}")
+
+    # altitude check
+    if z > -10:
+        print("altitude status: ⚠️ low altitude")
+
+    else:
+        print("altitude status: ✅ safe altitude")
+
+
+    # hover detection
+
+    if abs(vx) < 0.1 and abs(vy) < 0.1 and abs(vz) < 0.1:
+        print("flight Status: 🟩 Hovering ")
+
+    else:
+        print("Flight Status: reaching for the stars oluwafemi")
+
+        time.sleep(1)
